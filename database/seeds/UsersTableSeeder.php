e8c3c583f2b11aedb285fe52d2ed87c9b1d629bc
Daniel Sundermeier <daniel@hof-sundermeier.de>
2020-10-31 07:49:58 +0100

    updated: UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,30 +17,10 @@ class UsersTableSeeder extends Seeder
             'company_id' => 1,
             'email' => 'daniel@rechnungspilot.de',
             'firstname' => 'Daniel',
-            'password' => Hash::make('flash3'),
+            'password' => Hash::make('password'),
             'lastname' => 'Sundermeier',
         ]);
 
         $user->syncRoles(1);
-
-        $user = factory(App\User::class)->create([
-            'company_id' => Company::first()->id,
-            'email' => 'mussmann@mussmann-partner.net',
-            'firstname' => 'Olaf',
-            'password' => Hash::make('olaf'),
-            'lastname' => 'Mußmann',
-        ]);
-
-        $user->syncRoles('admin');
-
-        $user = factory(App\User::class)->create([
-            'company_id' => 2,
-            'email' => 'info@serienguide.tv',
-            'firstname' => 'Daniel',
-            'password' => Hash::make('flash3'),
-            'lastname' => 'Sundermeier',
-        ]);
-
-        $user->syncRoles(2);
     }
 }
