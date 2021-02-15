21b44f0fe18227d7fdcf363414c96723d3fc5b7c
Daniel Sundermeier <daniel@hof-sundermeier.de>
2021-02-15 11:19:47 +0100

    Update app.blade.php

--- resources/views/hof-sundermeier/app.blade.php
+++ resources/views/hof-sundermeier/app.blade.php
@@ -49,8 +49,8 @@
         </div>
         <div class="hidden md:flex md:space-x-10">
           <a href="#contact" class="font-medium text-gray-500 hover:text-gray-900">Kontakt</a>
-          <a href="https://danielsundermeier.gitbook.io/knowledge/" class="font-medium text-gray-500 hover:text-gray-900">Notizen</a>
-          <a href="https://github.com/danielsundermeier" class="font-medium text-gray-500 hover:text-gray-900">Github</a>
+          <a href="#bluehflaechen" class="font-medium text-gray-500 hover:text-gray-900">Blühflächen</a>
+          <a href="#weidehaehnchen" class="font-medium text-gray-500 hover:text-gray-900">Weidehähnchen</a>
         </div>
       </nav>
     </div>
@@ -69,7 +69,7 @@
         <div class="px-5 pt-4 flex items-center justify-between">
           <div>
               <a href="/">
-                  <span class="text-3xl font-bold text-indigo-600">D15r</span>
+                  <span class="text-3xl font-bold text-indigo-600">Hof Sundermeier</span>
               </a>
           </div>
           <div class="-mr-2">
@@ -85,8 +85,8 @@
         <div role="menu" aria-orientation="vertical" aria-labelledby="main-menu">
           <div class="px-2 pt-2 pb-3" role="none">
             <a href="#contact" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Kontakt</a>
-            <a href="https://danielsundermeier.gitbook.io/knowledge/" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Notizen</a>
-            <a href="https://github.com/danielsundermeier" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Github</a>
+            <a href="#bluehflaechen" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Blühflächen</a>
+            <a href="#weidehaehnchen" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Weidehähnchen</a>
           </div>
         </div>
       </div>
@@ -98,110 +98,198 @@
   <div class="px-4 sm:px-6 lg:px-8">
     <div class="text-lg max-w-prose mx-auto text-center">
       <h1>
-        <span class="mt-2 block text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Hi, ich bin Daniel</span>
+        <span class="mt-2 block text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Herzlich Willkommen bei Hof Sundermeier
+        </span>
       </h1>
       <p class="mt-8 text-xl text-gray-500 leading-8">
-        Es macht mir Spaß mein Leben mit <a href="#software" class="underline hover:text-gray-500 hover:bg-gray-100">Software</a> und <a href="#automation" class="underline hover:text-gray-500 hover:bg-gray-100">Automatisierungen</a> zu vereinfachen.
-        Ich möchte alles automatisieren, damit sich jeder auf seine Leidenschaft konzentrieren kann (<a href="https://danielsundermeier.gitbook.io/knowledge/masterplan/masterplan" class="underline hover:text-gray-500 hover:bg-gray-100">Masterplan</a>).
-        Ich versuche mein <a href="https://danielsundermeier.gitbook.io/knowledge/leben/leben" class="underline hover:text-gray-500 hover:bg-gray-100">Leben</a> so einfach und angenehm wie möglich zu gestalten.
-        Ich veröffentliche meine Gedanken in meinen <a href="https://danielsundermeier.gitbook.io/knowledge/" class="underline hover:text-gray-500 hover:bg-gray-100">Notizen</a>. Du bist herzlich eingeladen mit mir darüber zu <a href="#contact" class="underline hover:text-gray-500 hover:bg-gray-100">diskutieren</a>.
-        Meine anderen Projekte findest Du bei <a href="https://github.com/danielsundermeier" class="underline hover:text-gray-500 hover:bg-gray-100">Github</a>.
+          Familienbetrieb, Blühflächen, Weidehähnchen. Familienfoto im Hintergrund
       </p>
     </div>
   </div>
 </div>
 
-<div class="bg-white" id="software">
-  <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
-    <div>
-      <h2 class="text-base font-semibold text-indigo-600 uppercase tracking-wide">Programmieren</h2>
-      <p class="mt-2 text-3xl font-extrabold text-gray-900">Auswahl meiner Projekte</p>
-      <p class="mt-4 text-lg text-gray-500"></p>
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="py-16 bg-gray-50 overflow-hidden lg:py-24">
+  <div class="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+    <svg class="hidden lg:block absolute left-full transform -translate-x-1/2 -translate-y-1/4" width="404" height="784" fill="none" viewBox="0 0 404 784" aria-hidden="true">
+      <defs>
+        <pattern id="b1e6e422-73f8-40a6-b5d9-c8586e37e0e7" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+          <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+        </pattern>
+      </defs>
+      <rect width="404" height="784" fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)" />
+    </svg>
+
+    <div class="relative">
+      <h2 class="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        Unsere Produkte
+      </h2>
+      <p class="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in, accusamus quisquam.
+      </p>
     </div>
-    <div class="mt-12 lg:mt-0 lg:col-span-2">
-      <dl class="space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:grid-rows-2 sm:grid-flow-col sm:gap-x-6 sm:gap-y-10 lg:gap-x-8">
-        <div class="flex">
-          <div class="ml-3">
-            <dt class="text-lg leading-6 font-medium text-gray-900">
-              <a href="https://www.serienguide.tv" target="_blank">serienguide.tv</a>
-            </dt>
-            <dd class="mt-2 text-base text-gray-500">
-              Markiere deine gesehenen Filme & Episoden als gesehen, verwalte Listen und folge deinen Freunden.
-            </dd>
+
+    <div class="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center" id="bluehflaechen">
+      <div class="relative">
+        <h3 class="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+          Blühflächen
+        </h3>
+        <p class="mt-3 text-lg text-gray-500">
+          Werden Sie Parte von unseren Blühflächen und helfen sie der Natur
+        </p>
+
+        <dl class="mt-10 space-y-10">
+          <div class="flex">
+            <div class="flex-shrink-0">
+              <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                <!-- Heroicon name: outline/globe-alt -->
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                </svg>
+              </div>
+            </div>
+            <div class="ml-4">
+              <dt class="text-lg leading-6 font-medium text-gray-900">
+                Bieneweide
+              </dt>
+              <dd class="mt-2 text-base text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </dd>
+            </div>
           </div>
+
+          <div class="flex">
+            <div class="flex-shrink-0">
+              <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                <!-- Heroicon name: outline/scale -->
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+                </svg>
+              </div>
+            </div>
+            <div class="ml-4">
+              <dt class="text-lg leading-6 font-medium text-gray-900">
+                Unser Dorf soll schöner Werden
+              </dt>
+              <dd class="mt-2 text-base text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </dd>
+            </div>
+          </div>
+
+          <div class="flex">
+            <div class="flex-shrink-0">
+              <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                <!-- Heroicon name: outline/lightning-bolt -->
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                </svg>
+              </div>
+            </div>
+            <div class="ml-4">
+              <dt class="text-lg leading-6 font-medium text-gray-900">
+                Vorteil 3
+              </dt>
+              <dd class="mt-2 text-base text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </dd>
+            </div>
+          </div>
+        </dl>
+      </div>
+
+      <div class="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
+        <svg class="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden" width="784" height="404" fill="none" viewBox="0 0 784 404">
+          <defs>
+            <pattern id="ca9667ae-9f92-4be7-abcb-9e3d727f2941" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+              <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width="784" height="404" fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)" />
+        </svg>
+        Bild von Blühfläche
+        <img class="relative mx-auto" width="490" src="https://tailwindui.com/img/features/feature-example-1.png" alt="">
+      </div>
+    </div>
+
+    <svg class="hidden lg:block absolute right-full transform translate-x-1/2 translate-y-12" width="404" height="784" fill="none" viewBox="0 0 404 784" aria-hidden="true">
+      <defs>
+        <pattern id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+          <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+        </pattern>
+      </defs>
+      <rect width="404" height="784" fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
+    </svg>
+
+    <div class="relative mt-12 sm:mt-16 lg:mt-24" id="weidehaehnchen">
+      <div class="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
+        <div class="lg:col-start-2">
+          <h3 class="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+            Weidehähnchen
+          </h3>
+          <p class="mt-3 text-lg text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ex obcaecati natus eligendi delectus, cum deleniti sunt in labore nihil quod quibusdam expedita nemo.
+          </p>
+
+          <dl class="mt-10 space-y-10">
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <!-- Heroicon name: outline/annotation -->
+                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                  </svg>
+                </div>
+              </div>
+              <div class="ml-4">
+                <dt class="text-lg leading-6 font-medium text-gray-900">
+                  Lecker
+                </dt>
+                <dd class="mt-2 text-base text-gray-500">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                </dd>
+              </div>
+            </div>
+
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <!-- Heroicon name: outline/mail -->
+                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                  </svg>
+                </div>
+              </div>
+              <div class="ml-4">
+                <dt class="text-lg leading-6 font-medium text-gray-900">
+                  Artgerechte Haltung
+                </dt>
+                <dd class="mt-2 text-base text-gray-500">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                </dd>
+              </div>
+            </div>
+          </dl>
         </div>
 
-        <div class="flex">
-          <div class="ml-3">
-            <dt class="text-lg leading-6 font-medium text-gray-900">
-              <a href="https://www.rechnungspilot.de" target="_blank">Rechnungspilot</a>
-            </dt>
-            <dd class="mt-2 text-base text-gray-500">
-              Verwalte deine Firma, CRM, Rechnungen schreiben, etc.
-            </dd>
-          </div>
+        <div class="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
+          <svg class="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden" width="784" height="404" fill="none" viewBox="0 0 784 404" aria-hidden="true">
+            <defs>
+              <pattern id="e80155a9-dfde-425a-b5ea-1f6fadd20131" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+              </pattern>
+            </defs>
+            <rect width="784" height="404" fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)" />
+          </svg>
+          Bild von Weidehähnchen
+          <img class="relative mx-auto" width="490" src="https://tailwindui.com/img/features/feature-example-2.png" alt="">
         </div>
-
-        <div class="flex">
-          <div class="ml-3">
-            <dt class="text-lg leading-6 font-medium text-gray-900">
-              <a href="https://www.cardmonitor.de" target="_blank">Cardmonitor</a>
-            </dt>
-            <dd class="mt-2 text-base text-gray-500">
-              Verwaltung für dein Carmarket Konto.
-              Artikel verwalten, Bestellungen abwickeln und Preise aktualsieren.
-            </dd>
-          </div>
-        </div>
-
-        <div class="flex">
-          <div class="ml-3">
-            <dt class="text-lg leading-6 font-medium text-gray-900">
-              <a href="https://github.com/LifeOS-HQ" target="_blank">LifeOS</a>
-            </dt>
-            <dd class="mt-2 text-base text-gray-500">
-              Aggregieren von Daten aus meinem Leben.
-            </dd>
-          </div>
-        </div>
-      </dl>
+      </div>
     </div>
   </div>
 </div>
 
-<div class="bg-gray-300" id="automation">
-  <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
-    <div>
-      <h2 class="text-base font-semibold text-indigo-600 uppercase tracking-wide">Automation</h2>
-      <p class="mt-2 text-3xl font-extrabold text-gray-900">Tools, die ich nutze</p>
-      <p class="mt-4 text-lg text-gray-500"></p>
-    </div>
-    <div class="mt-12 lg:mt-0 lg:col-span-2">
-      <dl class="space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:grid-rows-1 sm:grid-flow-col sm:gap-x-6 sm:gap-y-10 lg:gap-x-8">
-        <div class="flex">
-          <div class="ml-3">
-            <dt class="text-lg leading-6 font-medium text-gray-900">
-              <a href="https://www.home-assistant.io/" target="_blank">Home Assistant</a>
-            </dt>
-            <dd class="mt-2 text-base text-gray-500">
-              Open source home automation that puts local control and privacy first.
-            </dd>
-          </div>
-        </div>
-
-        <div class="flex">
-          <div class="ml-3">
-            <dt class="text-lg leading-6 font-medium text-gray-900">
-              <a href="https://esphome.io/" target="_blank">ESPHome</a>
-            </dt>
-            <dd class="mt-2 text-base text-gray-500">
-              Firmaware für ESP8266, 32.
-            </dd>
-          </div>
-        </div>
-      </dl>
-  </div>
-</div>
 
 <div id="contact" class="relative bg-white">
   <div class="absolute inset-0">
@@ -214,7 +302,7 @@
           Sag Hallo
         </h2>
         <p class="mt-3 text-lg leading-6 text-gray-500">
-          Ich unterstütze dich gerne bei der Programmierung oder Automatisierung von Abläufen!
+          Haben Sie Fragen oder Anregungen? Nehmen Sie Kontakt auf
         </p>
       </div>
     </div>
