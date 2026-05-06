6063940cfca67f2b3e7e5c70a5030deef649c5d4
danielsundermeier <daniel@hof-sundermeier.de>
2026-05-06 13:28:49 +0200

    Upgrade deps; use framework TrustProxies
    Bump Composer dependencies (including Laravel to ^13 and multiple package version upgrades) and regenerate composer.lock. Remove fideloper/proxy and spatie/laravel-cookie-consent usage: replace the app's custom App\Http\Middleware\TrustProxies with Illuminate\Http\Middleware\TrustProxies in Kernel and delete the custom middleware file. Run composer update/install and verify PHP version compatibility and application tests after these upgrades.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,8 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
-        \Spatie\CookieConsent\CookieConsentMiddleware::class,
+        \Illuminate\Http\Middleware\TrustProxies::class,
     ];
 
     /**
